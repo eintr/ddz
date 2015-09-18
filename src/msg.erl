023a3051b49b5cc96f6fe5ef1a317884927ea8e9
牛海青 <haiqing1@staff.sina.com.cn>
2015-09-18 13:52:08 +0800

    Removed temp log.

--- src/msg.erl
+++ src/msg.erl
@@ -72,7 +72,6 @@ decode(MsgBin) ->
 	end.
 
 encrypt_keysync(Info, {_PrivK, PubK}) ->
-	io:format("encrypt_keysync ~p\n", [Info]),
 	PlainBin = <<	(Info#msg_body_keysync_info.client_id):4/binary,
 					(Info#msg_body_keysync_info.shared_key):8/binary,
 					(str_to_nbin(Info#msg_body_keysync_info.username, 32)):32/binary,
